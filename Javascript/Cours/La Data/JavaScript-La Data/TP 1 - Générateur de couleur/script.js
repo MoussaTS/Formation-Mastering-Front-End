20b49e2ef32361c53447cd3e32e48e5a85f94ce9
Moussa <moussa.tayaa.dev@gmail.com>
2024-03-26 21:14:05 +0100

    tp 1 color generator

---
+++ Javascript/Cours/La Data/JavaScript-La Data/TP 1 - Générateur de couleur/script.js
@@ -0,0 +1,12 @@
+function getColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  const color = `rgb(${red}, ${green}, ${blue})`;
+
+  document.body.style.background = color;
+  document.body.innerHTML = `<h2>${color}</h2>`;
+}
+
+setInterval(getColor, 1500);
