883daa5762074b984c6d1ede9c9a9d1545e4b1c7
Moussa <moussa.tayaa.dev@gmail.com>
2024-03-29 23:29:58 +0100

    destructuration

--- Javascript/Cours/API/API-Cours/joke-app/script.js
+++ Javascript/Cours/API/API-Cours/joke-app/script.js
@@ -1,23 +1,20 @@
 // https://api.blablagues.net/?rub=blagues
 
 const header = document.getElementById("header");
-const content = document.getElementById("content");
-const app = document.querySelector(".app");
+const text = document.getElementById("content");
 
 function getJoke() {
   fetch("https://api.blablagues.net/?rub=blagues")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.data.content);
+      const content = data.data.content;
+      console.log(content);
 
-      header.textContent = data.data.content.text_head;
-      content.textContent =
-        data.data.content.text !== ""
-          ? data.data.content.text
-          : data.data.content.text_hidden;
+      header.textContent = content.text_head;
+      text.textContent =
+        content.text !== "" ? content.text : content.text_hidden;
     });
 }
 
-// Ajouter un événement de clic à l'élément .app
-app.addEventListener("click", getJoke);
+document.body.addEventListener("click", getJoke);
 getJoke();
